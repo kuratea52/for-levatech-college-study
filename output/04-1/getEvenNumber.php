a4ce791c0b5f233d983141f5024e28adf8f24098
kuratea52 <yasaimogu@gmail.com>
2024-01-10 10:09:16 +0900

    add

--- output/04-1/getEvenNumber.php
+++ output/04-1/getEvenNumber.php
@@ -3,7 +3,7 @@
     // ０番目の値はファイル名なので、１番目を指定する
     $input_num = $argv[1];
 
-    if ($input_num % 2 === 0) {
+    if ($input_num % 2 == 0) {
         echo $input_num . "は偶数です" . PHP_EOL;
     } else {
         echo $input_num . "は奇数です" . PHP_EOL;
