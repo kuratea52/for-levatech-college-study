e1ad299d16785ed07a4786131756d0eb07e367ea
kuratea52 <yasaimogu@gmail.com>
2024-01-10 10:19:52 +0900

    add

--- output/04-1/input.php
+++ output/04-1/input.php
@@ -1,8 +1,8 @@
 <?php
-$numbers = array();   // array()で空の配列を作る
+$numbers = array();   // 「array()」で空の配列を作る
 
 for ($i = 1; $i <= 100; $i++) {
-    // array_push(追加先の配列名, 追加する値)
+    // 「array_push(追加先の配列名, 追加する値)」で配列に要素を追加
     array_push($numbers, $i);
 }
 ?>
