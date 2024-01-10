77480f4ce0ab8ee4f7fd338ed93644fe37aeffb7
kuratea52 <yasaimogu@gmail.com>
2024-01-10 11:01:38 +0900

    add

--- output/04-1/output.php
+++ output/04-1/output.php
@@ -3,6 +3,8 @@ require "./input.php";   // 「require "ファイルパス")」で外部ファ�
 
 // 「foreach(配列変数 as 変数)」で配列の要素で繰り返し処理
 foreach ($numbers as $num) {
+    // 最初に３の倍数にも５の倍数にも当てはまる条件を指定する
+    // 最初の条件を３の倍数のみ当てはまる式にした場合、１５は「Fizz」のままになってしまう
     if ($num % 3 == 0 && $num % 5 == 0) {
         echo "FizzBuzz";
     } elseif ($num % 3 == 0) {
