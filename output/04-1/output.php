4bafa457502dca13468d6ec96303f8d0978a6997
kuratea52 <yasaimogu@gmail.com>
2024-01-10 10:32:06 +0900

    add

--- output/04-1/output.php
+++ output/04-1/output.php
@@ -2,7 +2,9 @@
 require "./input.php";   // 「require "ファイルパス")」で外部ファイル読み込み
 
 // 「foreach(配列変数 as 変数)」で配列の要素で繰り返し処理
-foreach ($numbers as $number) {
-
+foreach ($numbers as $num) {
+    if ($num % 3 == 0) {
+        echo "Fizz";
+    }
 }
 ?>
